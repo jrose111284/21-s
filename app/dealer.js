8cf8df43e9d3454cfd1e72606da6ad6abd18568d
jamie rose <jrose111284@gmail.com>
2016-03-16 12:00:41 +0000

    will add the cards together not including ace,jack,queen king

--- app/dealer.js
+++ app/dealer.js
@@ -5,7 +5,14 @@ var Dealer = function() {
 };
 
 Dealer.prototype.dealCard = function() {
-return 'your card is ' + deck.dealCardNumber() + ' of ' + deck.dealsuite();
+  return 'your card is ' + deck.dealCardNumber() + ' of ' + deck.dealsuite();
 };
+
+Dealer.prototype.calculateCards = function(){
+  var total = [];
+  total.push(deck.dealCardNumber());
+  // for (var i = 0, sum = 0; i < total.length; sum += total[i++]);
+  return total;
+}
 
 module.exports = Dealer;
--- spec/dealerspec.js
+++ spec/dealerspec.js
@@ -7,6 +7,7 @@ beforeEach(function() {
 });
 
 describe('dealer dealing cards', function() {
+
   it('deals a card with a suite', function(){
     spyOn(Math,'random').and.returnValues(0.48, 0.49);
     expect(dealer.dealCard()).toEqual('your card is 7 of Clubs');
@@ -21,5 +22,13 @@ describe('dealer dealing cards', function() {
     spyOn(Math,'random').and.returnValues(0.48, 0.49, 0.88, 0.74);
     expect(dealer.dealCard()).toEqual('your card is 7 of Clubs');
     expect(dealer.dealCard()).toEqual('your card is Queen of Hearts');
-  })
+  });
+});
+
+describe('will added the two cards together', function(){
+
+  it('will add the cards together not including ace,jack,queen king', function(){
+    spyOn(Math,'random').and.returnValue(0.72);
+    expect(dealer.calculateCards()).toEqual([10]);
+  });
 });
